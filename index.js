f4fd2276f7df1d4a35fcdd376f72a5ca4a4d3191
Eduardo Enrique Garay Avilés <eduardo.avil3s@gmail.com>
2020-06-30 23:43:32 -0600

    Versión sin el condicional para los datos NaN

---
+++ index.js
@@ -0,0 +1,53 @@
+console.log('Programa ejecutado... Esperando su siguiente orden.');
+
+function valideKey(evt) {
+var code = evt.which ? evt.which : evt.keyCode;
+	if (code == 8) {
+		//backspace
+		return true;
+	} else if (code >= 48 && code <= 57) {
+		//is a number
+		return true;
+	} else {
+		return false;
+	}
+}	
+
+function plus() {
+	var uno = document.getElementById('n1');
+	var dos = document.getElementById('n2');
+	var total;
+	total = parseInt(uno.value) + parseInt(dos.value);
+	document.getElementById("result").value = total;
+}
+
+function less() {
+	var uno = document.getElementById('n1');
+	var dos = document.getElementById('n2');
+	var total;
+	total = parseInt(uno.value) - parseInt(dos.value);
+	document.getElementById("result").value = total;
+}
+
+function mult() {
+	var uno = document.getElementById('n1');
+	var dos = document.getElementById('n2');
+	var total;
+	total = parseInt(uno.value) * parseInt(dos.value);
+	document.getElementById("result").value = total;
+}
+
+function split() {
+	var uno = document.getElementById('n1');
+	var dos = document.getElementById('n2');
+	total = parseInt(uno.value) / parseInt(dos.value);
+	if (total == Infinity){
+		document.getElementById("result").value = 'No se puede operar con cero.';
+	} else {
+		var uno = document.getElementById('n1');
+		var dos = document.getElementById('n2');
+		var total;
+		total = parseInt(uno.value) / parseInt(dos.value);
+		document.getElementById("result").value = total;
+	}
+}
